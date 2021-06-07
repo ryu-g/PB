2a1ed0104ba130e7b6b9d32f00f2ec97c29f4484
ryu-g <em.ryu.g@gmail.com>
2021-06-07 20:12:25 +0900

    change log-format

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 const test = 'test text'
 const outFileName = 'test.md'
 
-let numA = random(128) 
+let numA = random(120) 
 let numB = random(128) 
-let numC = random(128) 
+let numC = random(128)
 
 const reWrite = (value, fileName) =>{
   fs.writeFile(fileName, value, (err, data) => {
@@ -14,26 +14,32 @@ const reWrite = (value, fileName) =>{
   })
 }
 
-const appendWrite = (value, fileName) => {
-  fs.appendFileSync(fileName, `${value} \n`)
+const appendWrite = (a, b, c, sum, fileName) => {
+  fs.appendFileSync(fileName, `${sum}, ${a}, ${b}, ${c} \n`)
+  console.log("----SAVED VALUE----")
 }
 
-appendWrite(test, outFileName)
-
-for (let i = 0; i < 999 ; i ++ ){
-  numA = random(128) 
-  numB = random(128) 
-  numC = random(128) 
-
-  let result = numA**3n - numB**3n + numC**3n
-  console.log(`[${i}]result is ${result}`)
-  if(result == 114n || result == 165n || result == 390n ||result == 579n || result == 627n || result == 633n || result == 732n || result == 906n || result == 921n || result == 975n){
-    console.log(`numA = ${numA}`)
-    console.log(`numB = ${numB}`)
-    console.log(`numC = ${numC}`)
-    i = 999
+const solve = (num) =>{
+  for (let i = 0; i < num ; i ++ ){
+    numA = random(72) 
+    numB = random(72)
+    numC = random(70) 
+    let result =  numA**3n - numB**3n - numC**3n
+    console.log(`[${i}]result is ${result}`)
+    if(result.toString().length < 5 ||
+      result == 114n || result == 165n || result == 390n ||result == 579n || result == 627n || result == 633n || result == 732n || result == 906n || result == 921n || result == 975n){
+      console.log(`numA = ${numA}`)
+      console.log(`numB = ${numB}`)
+      console.log(`numC = ${numC}`)
+      appendWrite(numA, numB, numC, result, outFileName)
+      i = 999
+    }
   }
 }
+
+solve(1500000)
+console.log(`${numA},\n${numB},\n${numC}`)
+console.log(numA.toString().length)
 
 random(128, function(err, num) {
   if (err)
